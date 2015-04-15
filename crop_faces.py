b3f7999502ddf6c317f6ea6e2c3e5f5896ecfd19
tabe <tabe@preferred.jp>
2015-04-15 18:14:32 +0900

    Add crop_faces.py

---
+++ crop_faces.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+from skimage import io, transform
+from glob import iglob
+
+def main():
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    for i, image_path in enumerate(iglob('%s/*/*.jpg' % input_dir)):
+        annotation_path = '%s.cat' % image_path
+        annotation = parse_annotation(open(annotation_path).read())
+        face = crop_face(io.imread(image_path), annotation)
+        if face != None:
+            io.imsave('%s/%d.png' % (output_dir, i), face)
+
+def parse_annotation(line):
+    v = map(int, line.split())
+    ret = {}
+    parts = ["left_eye", "right_eye", "mouth",
+             "left_ear1", "left_ear2", "left_ear3",
+             "right_ear1", "right_ear2", "right_ear3"]
+    for i, part in enumerate(parts):
+        if i >= v[0]: break
+        ret[part] = np.array([v[1 + 2 * i], v[1 + 2 * i + 1]])
+    return ret
+
+def crop_face(image, an):
+    center = (an["left_eye"] + an["right_eye"] + an["mouth"]) / 3
+    radius = np.linalg.norm(an["left_eye"] - an["right_eye"])
+    xu = center[0] - radius
+    xl = center[0] + radius
+    yu = center[1] - radius
+    yl = center[1] + radius
+    if xu < 0 or yu < 0: return None
+    if xl > image.shape[1] or yl > image.shape[0]:
+        return None
+    cropped = image[yu:yl, xu:xl]
+    return transform.resize(cropped, (64, 64))
+
+if __name__ == "__main__":
+    main()
